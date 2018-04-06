b551bd7cde2e6f00a58aaaa9719dc22ba2b8bd5c
shichengyuan <15036062755@163.com>
2018-04-06 16:14:08 +0800

    config qiniu

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,7 +4,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+   # storage :file
+
+   storage :qiniu
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
@@ -12,7 +14,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-  
+
    process resize_to_fit: [800, 800]
 
  version :thumb do
